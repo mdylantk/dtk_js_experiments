f697c4b55662afcbf5cea9b8c139eae07881cdb1
Dylan <33075764+mdylantk@users.noreply.github.com>
2018-12-11 04:06:20 -0500

    Update DTKSlideshow.js
    Added a few more samples

--- DTKSlideshow.js
+++ DTKSlideshow.js
@@ -16,6 +16,41 @@ var DTKSlideshow = {
 			Src:"https://66.media.tumblr.com/c0b5418068bfb30906a0d860ec5bc414/tumblr_mgaig6lN9H1qcye5fo1_1280.jpg",
 			Source:"https://www.deviantart.com/mdylantk/art/Night-Reading-III-340970745",
 			Disc:"Black and White version"
+		},
+		{
+			Name:"Spigots I",
+			Author:"mdylantk",
+			Src:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/0073ab99-ff1d-4f40-a34b-530804cb7edc/daksxti-7aa61e81-aab1-47f1-8d60-5c3e9d61e6b2.png/v1/fill/w_738,h_1083,q_70,strp/spigots_i_by_mdylantk_daksxti-pre.jpg",
+			Source:"https://www.deviantart.com/mdylantk/art/spigots-I-639604278",
+			Disc:""
+		},
+		{
+			Name:"GEnB IV",
+			Author:"mdylantk",
+			Src:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/0073ab99-ff1d-4f40-a34b-530804cb7edc/daj3vj3-aa3025f4-25a5-4e81-a3cc-3e70526b8cc0.png/v1/fill/w_771,h_1036,q_70,strp/genb_iv_by_mdylantk_daj3vj3-pre.jpg",
+			Source:"https://www.deviantart.com/mdylantk/art/GEnB-IV-636755295",
+			Disc:""
+		},
+		{
+			Name:"Forthteen I",
+			Author:"mdylantk",
+			Src:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/0073ab99-ff1d-4f40-a34b-530804cb7edc/daiy1k8-314ec222-66eb-4da3-b5ca-d84517c3893a.png/v1/fill/w_1024,h_775,q_80,strp/forthteen_i_by_mdylantk_daiy1k8-fullview.jpg",
+			Source:"https://www.deviantart.com/mdylantk/art/Forthteen-I-636483176",
+			Disc:""
+		},
+		{
+			Name:"N/A Husky",
+			Author:"mdylantk",
+			Src:"https://66.media.tumblr.com/eeeb89962d303e4f145ad095f4165795/tumblr_o546681D1x1qcye5fo1_1280.jpg",
+			Source:"https://www.instagram.com/p/BDyDY2ngI26/",
+			Disc:""
+		},
+		{
+			Name:"Circles III",
+			Author:"mdylantk",
+			Src:"https://66.media.tumblr.com/2db68b1d023d8cbd0ee5c5f0a4594430/tumblr_o43pjpKide1qcye5fo1_1280.png",
+			Source:"https://www.deviantart.com/mdylantk/art/Circles-III-596852316",
+			Disc:""
 		}
 	],
 	Elements: {
